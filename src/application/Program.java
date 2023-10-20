85fcb94d1c321ff30f6d76de924987b323a5561c
Luis Henrique <kakahimita@gmail.com>
2023-10-20 07:24:43 -0300

    primeiro commit

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+/**
+ *
+ * @author kakah
+ */
+public class Program {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("hello world!!");
+
+    }
+}
